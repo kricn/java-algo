73823984f8b65e20005b9729308c12e811953cab
kricn <mnyt2436@126.com>
2022-06-05 23:50:19 +0800

    图 - 图的最短记录2暂存

--- src/Graph/Dijkstra.java
+++ src/Graph/Dijkstra.java
@@ -10,7 +10,7 @@ import java.util.Map.Entry;
  */
 public class Dijkstra {
 
-    public static HashMap<Node, Integer> Dijkstra(Node head) {
+    public static HashMap<Node, Integer> dijkstra(Node head) {
         // 从 head 出发到所有点的最小距离
         // key: 从 head 到 key，
         // value: 从 head 出发到 key 的最小距离
@@ -60,5 +60,87 @@ public class Dijkstra {
 
         return minNode;
     }
+
+    public static class NodeHeap {
+        // 将所有节点放入数组中
+        private Node[] nodes;
+        // 用于查询 node 在堆中的位置
+        private HashMap<Node, Integer> heapIndexMap;
+        // node 目前到 head 的最短距离
+        private HashMap<Node, Integer> distanceMap;
+        private int size;
+
+        public static void addOrUpdateOrIgnore(Node head, int index) {
+
+        }
+
+        public NodeHeap(int size) {
+            nodes = new Node[size];
+            heapIndexMap = new HashMap<Node, Integer>();
+            distanceMap = new HashMap<Node, Integer>();
+            size = 0;
+        }
+
+        // 往上调整为小根堆
+        private void insertHeapify(Node node, int index) {
+            while (distanceMap.get(nodes[index]) < distanceMap.get(nodes[(index - 1) / 2])) {
+                swap(index, (index - 1) / 2);
+                index = (index - 1) / 2;
+            }
+        }
+
+        // 往下调整成小根堆
+        private void heapify(int index, int size) {
+            int left = index * 2 + 1;
+            while (left < size) {
+                int smallest = left + 1 < size && distanceMap.get(nodes[left + 1]) < distanceMap.get(nodes[left]) ? left + 1 : left;
+                smallest = distanceMap.get(nodes[smallest]) < distanceMap.get(nodes[index]) ? smallest : index;
+            }
+        }
+
+        // 判断是否为空
+        private boolean isEmpty() {
+            return size == 0;
+        }
+
+        // 判断节点是否进入过堆
+        private boolean isEntered(Node node) {
+            return heapIndexMap.containsKey(node);
+        }
+
+        // 判断节点是否在堆上，不在堆上的节点会被标记为 -1，即表示该节点被弹出
+        private boolean inHeap(Node node) {
+            return isEntered(node) && heapIndexMap.get(node) != -1;
+        }
+
+        private void swap(int i, int j) {
+            heapIndexMap.put(nodes[i], j);
+            heapIndexMap.put(nodes[j], i);
+            Node tmp = nodes[i];
+            nodes[i] = nodes[j];
+            nodes[j] = tmp;
+        }
+    }
+
+    /**
+     * 改进后的dijkstra算法
+     * 利用小根堆加速
+     */
+    public static HashMap<Node, Integer> dijkstra2 (Node head, int size) {
+        NodeHeap nodeHeap = new NodeHeap(size);
+        nodeHeap.addOrUpdateOrIgnore(head, 0);
+        HashMap<Node, Integer> result = new HashMap<>();
+        while (!nodeHeap.isEmpty()) {
+            NodeRecord record = nodeHeap.pop();
+            Node cur = record.node;
+            int distance = record.distance;
+            for (Edge edge : cur.edges) {
+                nodeHeap.addOrUpdateOrIgnore(edge.to, edge.weight + distance);
+            }
+            result.put(cur, distance);
+        }
+        return result;
+    }
+
 
 }
